0d29b93d6b11b75e8df1c2ae7eabeb2601fb5615
Umair Abbas <321umairabbas@gmail.com>
2026-05-07 23:19:34 +0200

    initial structure + terraform backend

---
+++ terraform/backend.tf
@@ -0,0 +1,8 @@
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "rg-terraform-state"
+    storage_account_name = "umairdevopsstate"
+    container_name       = "tfstate"
+    key                  = "lab.terraform.tfstate"
+  }
+}
